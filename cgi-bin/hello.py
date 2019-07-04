1bf5bedc7b387401a2e49528d1474451fc28bdac
Maeda Yosuke <maedamaedamaeday@gmail.com>
2019-07-04 23:47:17 +0900

    first commit

---
+++ cgi-bin/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from numpy.random import *
+
+print("Content-Type: text/html\n")
+
+print('<!DOCTYPE html><html lang="ja"><head><title></title></head><body>')
+
+print( "<h1>Hello, world!</h1>" )
+print( rand(10) )
+
+print('</body></html>')
